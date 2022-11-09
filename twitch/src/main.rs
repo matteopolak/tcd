652b381ec986e9729578016e46c5e15bdf0ed715
matteopolak <matteopolak@hotmail.com>
2022-11-08 19:28:42 -0500

    feat: skip seen videos

--- twitch/src/main.rs
+++ twitch/src/main.rs
@@ -11,6 +11,7 @@ use clap::Parser;
 use cli::Args;
 use dotenv::dotenv;
 use futures::StreamExt;
+use prisma_client_rust::Direction;
 
 use crate::channel::{Channel, ChannelExt};
 
@@ -35,10 +36,30 @@ async fn main() {
 
 		channel.save(&client).await.unwrap();
 
+		let start_at = match client
+			.video()
+			.find_many(vec![prisma::video::WhereParam::AuthorIdEquals(channel.id)])
+			.order_by(prisma::video::OrderByParam::CreatedAt(Direction::Asc))
+			.take(1)
+			.exec()
+			.await
+			.unwrap()
+			.first()
+		{
+			Some(video) => video.created_at,
+			None => chrono::DateTime::<chrono::Utc>::MIN_UTC
+				.with_timezone(&chrono::FixedOffset::east(0)),
+		};
+
 		let mut video_iter = channel.get_videos();
 		let mut video_batch = video_iter.batch();
 
 		while let Some(videos) = video_batch.next().await {
+			// If they were all created before the newest stored video, skip it
+			if videos.iter().all(|v| v.created_at < start_at) {
+				break;
+			}
+
 			futures::stream::iter(videos.into_iter().map(|v| {
 				let download_comments_future = v.get_comments().download_all(&client, args.verbose);
 				let save_video_future = v.save(&client);
